a382b416c232e1484542ce959e8d98b485d1278a
Christoph <christoph@Christophs-MacBook-Pro.local>
2016-11-29 18:11:31 +0700

    no subroutine for x13 story

--- R/viewer.R
+++ R/viewer.R
@@ -6,15 +6,34 @@ standalone <- function(){
 }
 
 
-
-
 #' @export
-view <- function(x, story = NULL, quiet = TRUE){ 
+view <- function(x = NULL, story = NULL, quiet = TRUE){ 
 
   if (!is.null(story)){
     if (!require(x13story)){
       stop("The 'x13story' package is needed to display stories.\n\n  devtools::install_github('christophsax/x13story')")
     }
+
+    if (!grepl("\\.Rmd", story, ignore.case = TRUE)){
+      stop("File must have rmarkdown extension (.Rmd)")
+    }
+
+    # auto download from the internet
+    if (grepl("^http", story)){
+      tfile <- tempfile(fileext = ".Rmd")
+      download.file(story, tfile)
+      story <- tfile
+    }
+
+    story <- normalizePath(story)
+    .GlobalEnv$.story.passed.to.shiny <- x13story::parse_x13story(file = story)
+    on.exit(rm(.story.passed.to.shiny, envir=.GlobalEnv))
+
+    cat("Press ESC (or Ctrl-C) to get back to the R session\n")
+
+    wd <- system.file("app", package = "seasonalview")
+
+    shiny::runApp(wd, quiet = quiet)
 
     view_story(file = story, quiet = quiet)
     return(NULL)
@@ -33,44 +52,6 @@ view <- function(x, story = NULL, quiet = TRUE){
 
   shiny::runApp(wd, quiet = quiet)
 }
-
-
-
-
-#' Local Display of Interactive Stories
-#' @param file character, path to the rmarkdown file containing an X-13 story.
-#' @param quiet logical, should the output of shiny be suppressed
-#' @examples
-#' \dontrun{
-#' file <- system.file(package = "x13story", "stories", "x11.Rmd")
-#' view(story = file)
-#' }
-#' 
-view_story <- function(file, quiet = TRUE){ 
-
-  if (!grepl("\\.Rmd", file, ignore.case = TRUE)){
-    stop("File must have rmarkdown extension (.Rmd)")
-  }
-
-  # auto download from the internet
-  if (grepl("^http", file)){
-    tfile <- tempfile(fileext = ".Rmd")
-    download.file(file, tfile)
-    file <- tfile
-  }
-
-  file <- normalizePath(file)
-  .GlobalEnv$.story.passed.to.shiny <- x13story::parse_x13story(file = file)
-  on.exit(rm(.story.passed.to.shiny, envir=.GlobalEnv))
-
-  cat("Press ESC (or Ctrl-C) to get back to the R session\n")
-
-  wd <- system.file("app", package = "seasonalview")
-
-  shiny::runApp(wd, quiet = quiet)
-
-}
-
 
 
 
